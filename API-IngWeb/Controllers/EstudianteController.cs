9f35732959518207d713a05b39a0671204a90518
JuliFlores21 <julian.flores.armijos@udla.edu.ec>
2024-10-07 12:16:36 -0500

    Agregar archivos de proyecto.

---
+++ API-IngWeb/Controllers/EstudianteController.cs
@@ -0,0 +1,49 @@
+﻿using API_IngWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_IngWeb.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstudianteController : Controller
+    {
+        [HttpGet]
+        public ActionResult<List<Estudiante>> GetEstudiantes()
+        {
+            return EstudianteRepository.GetEstudiantes();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Estudiante> GetEstudiante(int id)
+        {
+            var estudiante = EstudianteRepository.GetEstudianteId(id);
+            if (estudiante == null)
+                return NotFound();
+            return estudiante;
+        }
+
+        [HttpPost]
+        public ActionResult<Estudiante> AgregarEstudiante(Estudiante estudiante)
+        {
+            EstudianteRepository.AgregarEstudiante(estudiante);
+            return CreatedAtAction(nameof(GetEstudiante), new { id = estudiante.Id }, estudiante);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult ActualizarEstudiante(int id, Estudiante estudiante)
+        {
+            if (id != estudiante.Id)
+                return BadRequest();
+            EstudianteRepository.ActualizarEstudiante(estudiante);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult EliminarEstudiante(int id)
+        {
+            EstudianteRepository.EliminarEstudiante(id);
+            return NoContent();
+        }
+    }
+}
---
+++ API-IngWeb/Models/Estudiante.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_IngWeb.Models
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public int Edad { get; set; }
+    }
+}
+
